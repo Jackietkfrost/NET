18d2acb6114e350f908c473d9528141299eba9a0
Jacquelinne Marcano <jacquelinnemarcano@gmail.com>
2023-09-12 10:51:24 -0400

    Update basics.js
    Moved functionality of resize to main process.
    Added userbox logic for index page. Will probably move it out of here later. Considering moving to Vue for the main framework, and see if we can get it working properly from there so we can layer these items into modular components and improve on what's there.

--- basics.js
+++ basics.js
@@ -1,28 +1,30 @@
+
 function loadListeners(){
-    //Turn these into IPC events that handle sending the requests to the main render process and executing the events there.
     document.getElementById("close").addEventListener("click",function(){
         window.titlebar.closeWindow();
         console.log('Sending event?');
     });
+    
     document.getElementById("minimize").addEventListener("click",function(){
         window.titlebar.minWindow();
     });
+    
     document.getElementById("maximize").addEventListener("click",function(){
         window.titlebar.maxWindow();
     });
+    
+    
 }
-function toggleMaximizeWindow(){
-    let window = remote.getCurrentWindow();
-    if(window.isMaximized()){
-        window.restore();
-    }
-    else{
-        window.maximize();
-    }
-}
+
 function start(){
     loadListeners();
 }
 document.addEventListener("DOMContentLoaded",function(){
     start();
+    let userBox = document.getElementById('user-info');
+    if(userBox) {
+        userBox.addEventListener("focusout", function(){
+            window.netVar.setUsername(userBox.value);
+        })
+    }
 })
